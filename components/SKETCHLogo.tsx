320eff61fa27abc79bbd687cecc4b86022c715b8
m88vander <m88vander@gmail.com>
2026-08-17 19:56:05 -0700

    Create SKETCHLogo inline component for logo replacement

---
+++ components/SKETCHLogo.tsx
@@ -0,0 +1,10 @@
+export const SKETCHLogo = () => {
+  return (
+    <img
+      src="nes1k.jpg"
+      alt="SKETCH"
+      className="inline-block h-6 w-auto align-middle mx-1"
+      style={{ display: 'inline', verticalAlign: 'middle' }}
+    />
+  )
+}
